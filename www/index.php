02a0310eb3f273abbb9ec13b40f62c36781dcba4
sataketatsuya1121 <tatsuyaenginner@gmail.com>
2021-11-18 09:57:44 +0900

    checked successfully ctf envirnoment completed

---
+++ www/index.php
@@ -0,0 +1,86 @@
+<?php
+/*
+
+FLAG_{EasySQLiForBeginners}
+
+*/
+
+// check mysql-server's ip address with command bellow.
+// docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
+
+ini_set('display_errors', 0);
+$DBuser = 'root';
+$DBpass = $_ENV['MYSQL_ROOT_PASSWORD'];
+$pdo = null;
+
+try{
+    $database = 'mysql:dbname=ctf;host=192.168.160.2';
+    $pdo = new PDO($database, $DBuser, $DBpass);
+} catch(PDOException $e) {
+    echo "Error: Unable to connect to MySQL. Error:\n $e";
+}
+
+if(isset($_POST["id"]) && $pdo){
+    $uq = $pdo->query("SELECT id,loginid,auth_bit,lasttime FROM users WHERE loginid = '{$_POST['id']}' AND password = '{$_POST['pw']}'")->fetchAll();
+    if(count($uq) > 0){$uq = $uq[0];}
+    if(isset($uq["auth_bit"])){
+        if($uq["auth_bit"] == "1023"){
+            print "If you want to read the Flag. Read ME!";
+            phpinfo();
+            exit();
+        }
+
+        printLoginForm($uq["loginid"] . " is Not Admin User.");
+        exit();
+    }
+    printLoginForm($_POST["id"] . "  Not Found");
+} else {
+    printLoginForm();
+}
+
+
+function printLoginForm($error=""){
+?>
+<!doctype html>
+<html>
+<head>
+<title>Login</title>
+<style>
+#wrapper {
+    width: 800px;
+    margin: 0 auto;
+    text-align : center ;
+    border:1px ridge;
+}
+fieldset {
+    border:0px;
+}
+</style>
+</head>
+<body>
+<div id="wrapper">
+<?php if($error != ""){print h($error); } ?>
+<form method="POST">
+    <fieldset>
+        <label>ID</label>
+        <input type="text" name="id" size="30">
+    </fieldset>
+    <fieldset>
+        <label>PW</label>
+        <input type="password" name="pw" size="30">
+    </fieldset>
+    <input type="submit" value="Login">
+</form>
+</div>
+</body>
+</html>
+<?php
+}
+
+
+function h($str){
+    return htmlspecialchars($str,ENT_QUOTES,"UTF-8");
+}
+
+?>
+
